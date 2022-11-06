909ec0e4763953f008140dd888fd1ce5287d5e22
nicolay <sirghiinicolai1@gmail.com>
2022-11-06 15:29:36 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+
+        //Level1: В Китае живет 1 411 778 724 человек,
+        // всего в мире 197 стран.
+        // Сколько бы человек жило наземле,
+        // если бы в каждой стране жило
+        // население, равное китайско?
+        long chinaPopulation = 1411778724;
+        int countries = 197;
+        long worldPopulation =( chinaPopulation   *   countries );
+        System.out.println(   worldPopulation  );
+
+    }
+}
